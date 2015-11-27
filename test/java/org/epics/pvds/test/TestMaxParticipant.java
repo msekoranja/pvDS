2e6801bd4398defa1c7d80d6637e8255aee3f9bf
Matej Sekoranja <matej.sekoranja@cosylab.com>
2015-11-27 14:28:17 +0100

    TestMaxParticipant: configurable nif

--- test/java/org/epics/pvds/test/TestMaxParticipant.java
+++ test/java/org/epics/pvds/test/TestMaxParticipant.java
@@ -9,6 +9,7 @@ public class TestMaxParticipant {
 
 	public static void main(String[] args) throws Throwable {
 		
+		final String mcastNIF = (args.length) > 0 ? args[0] : null;
 		final int maxParticipants = Protocol.MAX_PARTICIPANT_ID + 1;
 		ArrayList<RTPSParticipant> list = new ArrayList<RTPSParticipant>(maxParticipants);
 		int i = 0;
@@ -17,7 +18,7 @@ public class TestMaxParticipant {
 			while (i < maxParticipants)
 			{
 				// create participants with different group 
-				list.add(new RTPSParticipant(null, 0, i, false));
+				list.add(new RTPSParticipant(mcastNIF, 0, i, false));
 				i++;
 			}
 		} catch (Throwable th) {
@@ -27,7 +28,7 @@ public class TestMaxParticipant {
 		System.out.println("Sucessfully created " + i + " (out of " + maxParticipants + ") participants/groups.");
 		if (lastException != null)
 		{
-			System.out.println("Last try failed with exception:\n");
+			System.out.println("Last try failed with exception:");
 			lastException.printStackTrace(System.out);
 		}
 		
